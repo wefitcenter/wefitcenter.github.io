d3793bbe09e76d96d1ca60248bcd9e71b55ccff9
TechLife <wefitcenter@gmail.com>
2024-03-25 16:48:47 +0100

    js edits

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function clearInputs() {
     setsInput.value = ""
     repsInput.value = ""
     weightInput.value = ""
-    statusInput.value = ""
 } 
 
 function addToLocalStorage(){ 
@@ -17,7 +16,6 @@ function addToLocalStorage(){
     localStorage.setItem("sets", JSON.stringify(sets)) 
     localStorage.setItem("reps", JSON.stringify(reps)) 
     localStorage.setItem("weight", JSON.stringify(weight))
-    localStorage.setItem("status", JSON.stringify(status))
 } 
 
 function activateEdit(i){ 
@@ -25,7 +23,6 @@ function activateEdit(i){
     setsInput.value = sets[i] 
     repsInput.value = reps[i] 
     weightInput.value = weight[i] 
-    statusInput.value = status[i] 
     editIndex = i 
     submitButton.classList.add("hidden") 
     editSection.classList.remove("hidden") 
@@ -44,7 +41,6 @@ function editRow(){
     sets[editIndex] = setsInput.value 
     reps[editIndex] = repsInput.value 
     weight[editIndex] = weightInput.value 
-    status[editIndex] = statusInput.value 
     fillTable() 
     addToLocalStorage() 
     cancelEdit() 
@@ -59,7 +55,6 @@ function deleteRow(i){
     sets.splice(i, 1) 
     reps.splice(i, 1) 
     weight.splice(i, 1) 
-    status.splice(i, 1) 
     date.splice(i, 1) // Remove corresponding date entry
     document.querySelector(`#output > tr:nth-child(${i+1})`) 
         .classList.add("delete-animation") 
@@ -69,14 +64,13 @@ function deleteRow(i){
 
 function fillTable(){ 
     const tbody = document.getElementById("output") 
-    const rows = Math.max(exercise.length, sets.length, reps.length, weight.length, status.length, date.length) 
+    const rows = Math.max(exercise.length, sets.length, reps.length, weight.length, date.length) 
     let html = ""
     for(let i = 0; i < rows; i++){ 
         let e = exercise[i] || "N/A"
         let s = sets[i] || "N/A"
         let r = reps[i] || "N/A"
         let w = weight[i] || "N/A"
-        let st = status[i] || "N/A"
         let d = date[i] || "N/A" // Add date entry here
         html += `<tr> 
             <td>${d}</td> 
@@ -84,7 +78,6 @@ function fillTable(){
             <td>${s}</td> 
             <td>${r}</td> 
             <td>${w}</td>
-            <td>${st}</td>
             <td> 
                 <button onclick="activateEdit(${i})"
                         class="edit">${editIcon} 
@@ -108,13 +101,11 @@ let exercise = JSON.parse(localStorage.getItem("exercise")) || []
 let sets = JSON.parse(localStorage.getItem("sets")) || [] 
 let reps = JSON.parse(localStorage.getItem("reps")) || [] 
 let weight = JSON.parse(localStorage.getItem("weight")) || [] 
-let status = JSON.parse(localStorage.getItem("status")) || [] 
 
 const eInput = document.getElementById("exercise") 
 const setsInput = document.getElementById("sets") 
 const repsInput = document.getElementById("reps") 
 const weightInput = document.getElementById("weight") 
-const statusInput = document.getElementById("status") 
 
 const submitButton = document.getElementById("submit") 
 const editSection = document.getElementById("editSection") 
@@ -127,8 +118,7 @@ submitButton.addEventListener("click", ()=>{
     const s = setsInput.value || null; 
     const r = repsInput.value || null; 
     const w = weightInput.value || null; 
-    const st = statusInput.value || null; 
-    if(e===null || s===null || r===null || w===null || st===null) { 
+    if(e===null || s===null || r===null || w===null) { 
         alert("Please enter all the fields.") 
         return
     } 
@@ -136,7 +126,6 @@ submitButton.addEventListener("click", ()=>{
     sets.unshift(s) 
     reps.unshift(r) 
     weight.unshift(w) 
-    status.unshift(st) 
     let currentDate = new Date().toLocaleDateString(); // Get current date
     date.unshift(currentDate); // Add current date to the beginning
     clearInputs() 
